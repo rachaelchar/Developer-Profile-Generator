ef8936db44f5bac7666e4d76b787bd64eca99b27
RachaelChar <charewicz.r@gmail.com>
2019-11-18 08:49:02 -0500

    add questions

--- index.js
+++ index.js
@@ -2,9 +2,26 @@ const inquirer = require("inquirer");
 const axios = require("axios");
 const fs = require("fs");
 
-const questions = [
+const questions = [];
 
-];
+inquirer
+    .prompt([{
+        type: "input",
+        name: "username",
+        message: "What is your Github username?"
+    },
+    {
+        type: "list",
+        name: "color",
+        message: "What is your favorite color?",
+        choices:["red", "blue", "green", "yellow"]
+    },
+
+    ])
+    .then(function (answers){
+        console.log(answers);
+    }
+    );
 
 let url = `https://api.github.com/users/${username}`
 
