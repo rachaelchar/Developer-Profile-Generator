e9b6919f9e81325844a9cce45f8dadfc71652a2e
RachaelChar <charewicz.r@gmail.com>
2019-11-19 12:18:48 -0500

    get Github info by username in prompt call

--- index.js
+++ index.js
@@ -1,36 +1,62 @@
 const inquirer = require("inquirer");
 const axios = require("axios");
+// const util = require("util");
 const fs = require("fs");
+
 
 const questions = [];
 
-inquirer
-    .prompt([{
-        type: "input",
-        name: "username",
-        message: "What is your Github username?"
-    },
-    {
-        type: "list",
-        name: "color",
-        message: "What is your favorite color?",
-        choices:["green", "blue", "pink", "red"]
-    },
-    ])
-    .then(function ({username}){
-        const url = `https://api.github.com/users/${username}`;
+// const writeFileAsync = util.promisify(fs.writeFile);
 
-        axios.get(url).then(function(response) {
-            console.log(response.data);
-        });
-        
-    });
+function promptUser() {
+    return inquirer
+        .prompt([{
+            type: "input",
+            name: "username",
+            message: "What is your Github username?"
+        },
+        {
+            type: "list",
+            name: "color",
+            message: "What is your favorite color?",
+            choices: ["green", "blue", "pink", "red"]
+        },
+        ]);
+}
+
+promptUser()
+  .then(function(answers) {
+    console.log(answers);
+  })
+  .then(function(username) {
+    const url = `https://api.github.com/users/${username}`;
+
+    axios.get(url).then(function (response) {
+        console.log(response.data);
+    })
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
+
+
+
+
+
+
+//         fs.writeFile("portfolio.html", function (err) {
+//             if (err) {
+//                 throw err;
+//             }
+//         })
+//     });
+// });
 
 
 
 
 // function writeToFile(fileName, data) {
- 
+
 // }
 
 // function init() {}
